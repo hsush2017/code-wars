b7afa3cbf017eae82ae32577851621e083bd9a57
hsush <hsush@simbo.com.tw>
2023-10-21 13:03:25 +0800

    solve: String Split

---
+++ src/main/java/org/example/split_strings/StringSplit.java
@@ -0,0 +1,21 @@
+package org.example.split_strings;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringSplit {
+    public String[] solution(String s) {
+        if (s.length() % 2 > 0) {
+            s += "_";
+        }
+
+        List<String> result = new ArrayList<>();
+        while (!s.isEmpty()) {
+            String subStr = s.substring(0, 2);
+            s = s.replaceFirst(subStr, "");
+            result.add(subStr);
+        }
+
+        return result.toArray(new String[0]);
+    }
+}
