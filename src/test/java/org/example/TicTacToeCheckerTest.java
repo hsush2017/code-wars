a329e0d2f8e71d830a52a82882afdb0a85b5e4fa
hsush <hsush@simbo.com.tw>
2023-11-03 14:09:50 +0800

    solve: Tic-Tac-Toe Checker

---
+++ src/test/java/org/example/TicTacToeCheckerTest.java
@@ -0,0 +1,59 @@
+package org.example;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TicTacToeCheckerTest {
+    private TicTacToeChecker sut;
+
+    @Before
+    public void setUp() {
+        sut = new TicTacToeChecker();
+    }
+
+    @Test
+    public void notFinished() {
+        int[][] board = {
+                {0, 0, 1},
+                {0, 1, 2},
+                {2, 1, 0}
+        };
+
+        assertEquals(-1, sut.isSolved(board));
+    }
+
+    @Test
+    public void winnerX() {
+        int[][] board = {
+                {1, 1, 1},
+                {0, 2, 2},
+                {0, 0, 0}
+        };
+
+        assertEquals(1, sut.isSolved(board));
+    }
+
+    @Test
+    public void winnerO() {
+        int[][] board = {
+                {1, 1, 2},
+                {0, 2, 0},
+                {2, 1, 1}
+        };
+
+        assertEquals(2, sut.isSolved(board));
+    }
+
+    @Test
+    public void draw() {
+        int[][] board = {
+                {1, 2, 1},
+                {1, 1, 2},
+                {2, 1, 2}
+        };
+
+        assertEquals(0, sut.isSolved(board));
+    }
+}
